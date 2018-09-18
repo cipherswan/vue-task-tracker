05b7f7fd5a6d73eef5aeaa7ab8c911a4ba774440
cipherswan <leskauskas25@gmail.com>
2018-09-18 16:45:57 +0300

    Initial commit

---
+++ src/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div id="app">
+    <div class="header">
+      <h1>{{title}}</h1>
+      <h6>{{desc}}</h6>
+    </div>
+
+    <router-view/>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'App',
+  data() {
+    return {
+      title: 'Task Tracker',
+      desc: 'An application to track your everyday tasks.',
+    };
+  },
+};
+</script>
+
+<style>
+
+</style>
